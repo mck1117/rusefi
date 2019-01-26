f00ad32ca6429522abea53e0e61bac5460fe4ac7
Matthew Kennedy <matthewkennedy@outlook.com>
2019-01-26 03:32:45 -0800

    clarify

--- firmware/hw_layer/sensors/LambdaConverter.cpp
+++ firmware/hw_layer/sensors/LambdaConverter.cpp
@@ -6,16 +6,14 @@
 #define CJ125_PUMP_SHUNT_RESISTOR		(61.9f)
 #define CJ125_PUMP_CURRENT_FACTOR		(1000.0f)
 
-// Pump current, mA
-static const float cjLSU49Bins[] = { 
-	// LSU 4.9
-	-2.0f, -1.602f, -1.243f, -0.927f, -0.8f, -0.652f, -0.405f, -0.183f, -0.106f, -0.04f, 0, 0.015f, 0.097f, 0.193f, 0.250f, 0.329f, 0.671f, 0.938f, 1.150f, 1.385f, 1.700f, 2.000f, 2.150f, 2.250f
-};
-// Lambda value
-static const float cjLSU49Lambda[] = {
-	// LSU 4.9
-	0.65f, 0.7f, 0.75f, 0.8f, 0.822f, 0.85f, 0.9f, 0.95f, 0.97f, 0.99f, 1.003f, 1.01f, 1.05f, 1.1f, 1.132f, 1.179f, 1.429f, 1.701f, 1.990f, 2.434f, 3.413f, 5.391f, 7.506f, 10.119f
-};
+static constexpr float GetPumpCurrent(float vUaDelta, float gain)
+{
+    float currentPerVolt = CJ125_PUMP_CURRENT_FACTOR / CJ125_PUMP_SHUNT_RESISTOR / gain;
+
+	float pumpCurrent = vUaDelta * currentPerVolt;
+
+    return pumpCurrent;
+}
 
 // Pump current, mA
 static const float cjLSU42Bins[] = {
@@ -28,14 +26,6 @@ static const float cjLSU42Lambda[] = {
 	0.7f, 0.8f, 0.85f, 0.9f, 1.009f, 1.18f, 1.43f, 1.7f, 2.42f
 };
 
-static constexpr float GetPumpCurrent(float vUaDelta, float gain)
-{
-    float currentPerVolt = CJ125_PUMP_CURRENT_FACTOR / CJ125_PUMP_SHUNT_RESISTOR / gain;
-
-	float pumpCurrent = vUaDelta * currentPerVolt;
-
-    return pumpCurrent;
-}
 
 namespace LambdaConverterLsu42
 {
@@ -45,6 +35,17 @@ namespace LambdaConverterLsu42
 
         return interpolate2d("cj125Lsu49", pumpCurrent, cjLSU42Bins, cjLSU42Lambda, sizeof(cjLSU42Bins) / (sizeof(cjLSU42Bins[0])));
     }
+};
+
+// Pump current, mA
+static const float cjLSU49Bins[] = { 
+	// LSU 4.9
+	-2.0f, -1.602f, -1.243f, -0.927f, -0.8f, -0.652f, -0.405f, -0.183f, -0.106f, -0.04f, 0, 0.015f, 0.097f, 0.193f, 0.250f, 0.329f, 0.671f, 0.938f, 1.150f, 1.385f, 1.700f, 2.000f, 2.150f, 2.250f
+};
+// Lambda value
+static const float cjLSU49Lambda[] = {
+	// LSU 4.9
+	0.65f, 0.7f, 0.75f, 0.8f, 0.822f, 0.85f, 0.9f, 0.95f, 0.97f, 0.99f, 1.003f, 1.01f, 1.05f, 1.1f, 1.132f, 1.179f, 1.429f, 1.701f, 1.990f, 2.434f, 3.413f, 5.391f, 7.506f, 10.119f
 };
 
 namespace LambdaConverterLsu49
