135fa4137f7d757d92033a7b4a4d445c5ffad2f8
Matthew Kennedy <makenne@microsoft.com>
2020-08-12 22:15:10 -0700

    start pressure compensation

--- firmware/controllers/algo/fuel/injector_model.cpp
+++ firmware/controllers/algo/fuel/injector_model.cpp
@@ -12,10 +12,52 @@ constexpr float convertToGramsPerSecond(float ccPerMinute) {
 	return ccPerSecond * 0.72f;	// 0.72g/cc fuel density
 }
 
+float getConfiguredPressure() {
+	return 400;
+}
+
+float getCfgRailPressure() {
+	return 350;
+}
+
+float getActualRailPressure() {
+	return 500;
+}
+
 float InjectorModel::getInjectorMassFlowRate() const {
-	// TODO: injector flow dependent upon rail pressure (and temperature/ethanol content?)
-	auto injectorVolumeFlow = CONFIG(injector.flow);
-	return convertToGramsPerSecond(injectorVolumeFlow);
+	auto configuredFlow = convertToGramsPerSecond(CONFIG(injector.flow));
+
+	int mode = 0;
+
+	// 0 = - directly use configured flow rate
+	// 1 = - map-ref'd rail pressure, spec flow at known pressure (ie, injector is specified at some other pressure than you actually run)
+	// 2 = - constant fuel rail pressure, spec flow at known pressure (ie, NB miata where the press reg has no MAP ref)
+	// 3 = - sensed absolute fuel rail pressure, spec flow at known pressure (ie, GDI system with absolute fuel pressure sensor)
+	// 4 = - sensed relative fuel rail pressure, spec flow at known pressure (ie, Ford with MAP-relative fuel pressure sensor)
+
+	if (mode == 0) {
+		return configuredFlow;
+	}
+
+	// The injector has the configured flow at this pressure
+	float flowPressureReference = getConfiguredPressure();
+
+	float railPressure;
+
+	switch (mode) {
+	case 1:
+		railPressure = getCfgRailPressure();
+	case 2:
+		// Since rail pressure is constant, as MAP goes up, injector differential pressure drops
+		railPressure = getCfgRailPressure() - getMap();
+	case 3:
+		railPressure = getActualRailPressure() - getMap();
+	case 4:
+		railPressure = getActualRailPressure();
+	}
+
+	// Flow rate varies with the square root of the pressure - correct flow rate based on the pressure difference
+	return configuredFlow * sqrt(railPressure / flowPressureReference);
 }
 
 float InjectorModel::getDeadtime() const {
