a0081acbcaabd0b485a30cdbe1f0a086ccbbeeab
Matthew Kennedy <matthewkennedy@outlook.com>
2020-11-16 15:53:18 -0800

    pwm timer

---
+++ firmware/hw_layer/microsecond_timer_pwm.cpp
@@ -0,0 +1,50 @@
+#include "global.h"
+#include "os_access.h"
+#include "microsecond_timer.h"
+#include "os_util.h"
+
+bool hwStarted = false;
+
+void setHardwareSchedulerTimer(efitick_t nowNt, efitick_t setTimeNt) {
+	efiAssertVoid(OBD_PCM_Processor_Fault, hwStarted, "HW.started");
+
+	if (hasFirmwareError()) {
+		return;
+	}
+
+	pwm_lld_enable_channel(&PWMD5, 0, setTimeNt);
+	pwmEnableChannelNotificationI(&PWMD5, 0);
+}
+
+void globalTimerCallback();
+
+static void hwTimerCallback(PWMDriver*) {
+	pwmDisableChannelNotificationI(&PWMD5, 0);
+	globalTimerCallback();
+}
+
+static constexpr PWMConfig timerConfig = {
+	12'000'000,		// 12mhz was chosen because it's the GCD of (168, 180, 216), the three speeds of STM32 we support
+	(uint32_t)-1,	// timer period = 2^32 counts
+	nullptr,		// No update callback
+	{
+		{PWM_OUTPUT_DISABLED, hwTimerCallback},	// Channel 0 = timer callback, others unused
+		{PWM_OUTPUT_DISABLED, nullptr},
+		{PWM_OUTPUT_DISABLED, nullptr},
+		{PWM_OUTPUT_DISABLED, nullptr}
+	},
+	0,	// CR1
+	0	// CR2
+};
+
+void initMicrosecondTimer() {
+	pwmStart(&PWMD5, &timerConfig);
+
+	// ChibiOS doesn't let you configure timers in output compare mode, only PWM mode.
+	// We want to be able to set the compare register without waiting for an update event
+	// (which would take 358 seconds at 12mhz timer speed), so we have to use normal upcounting
+	// output compare mode instead.
+	TIM5->CCMR1 = 0x00006810;
+
+	hwStarted = true;
+}
